a3d0a8739353ad351ec4880331a89d39766b6d41
Bryan C Guner <bryan.guner@gmail.com>
2023-10-12 11:31:26 -0400

    rendering inner blocks with BlockRendererProvider

--- the-gatsby-garadge/src/templates/page.js
+++ the-gatsby-garadge/src/templates/page.js
@@ -1,12 +1,36 @@
 import React from "react";
-import { BlockRendererProvider } from "@webdeveducation/wp-block-tools";
-function page(props) {
+import {
+  BlockRendererProvider,
+  BlockRenderer,
+} from "@webdeveducation/wp-block-tools";
+
+function Page(props) {
   console.log(props);
+  
+
+
   return (
     <div>
-      <BlockRendererProvider allBlocks={props.pageContext.blocks} />
+      <BlockRendererProvider
+        allBlocks={props.pageContext.blocks}
+        renderComponent={(block) => {
+          console.log("Render Component:", block);
+          switch (block.name) {
+            case "core/media-text":
+              return (
+                <div key={block.id}>
+                  <BlockRenderer blocks={block.innerBlocks} />
+                </div>
+              );
+            default:
+              return null;
+          }
+        }}
+      >
+
+      </BlockRendererProvider>
     </div>
   );
 }
 
-export default page;
+export default Page;
